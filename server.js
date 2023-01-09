68347da13def7e48f3e94bef91e767c40cf70095
Yadhu <yadhu621@gmail.com>
2023-01-09 10:37:51 +0000

    use process.env.PORT

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const app = express();
+const PORT = process.env.PORT 
 
 app.get('/', (req,res) => {
   res.send('Hello World from Azure AppService');
 });
 
-app.listen(3000, () => {
-  console.log('Listening on port 3000...')
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}...`)
 });
